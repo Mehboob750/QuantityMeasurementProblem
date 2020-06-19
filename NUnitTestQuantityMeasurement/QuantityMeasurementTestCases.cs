bbce431e438b63d2a7e81f2b8acdbeb13dd9e066
Mehboob750 <ms568203@gmail.com>
2020-06-19 08:44:15 +0530

    Added: Code and Test to Check the Equality of Two Feets

---
+++ NUnitTestQuantityMeasurement/QuantityMeasurementTestCases.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+namespace NUnitTestQuantityMeasurement
+{
+    using QuantityMeasurementProblem;
+
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Given Zero Feet And Zero Feet Returns the Equal
+        /// </summary>
+        [Test]
+        public void Given0FeetAnd0Feet_ShouldReturnsEqual()
+        {
+            Feet firstFeet = new Feet(0.0);
+            Feet secondFeet = new Feet(0.0);
+            Assert.AreEqual(firstFeet,secondFeet);
+        }
+    }
+}
--- QuantityMeasurementProblem/Program.cs
+++ QuantityMeasurementProblem/Program.cs
@@ -6,7 +6,7 @@ namespace QuantityMeasurementProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome To Quantity Measurement Problem!!!");
         }
     }
 }
