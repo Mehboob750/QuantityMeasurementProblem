5e72f51661420700c2a41aa02313d3a5ad2dc19e
Mehboob750 <ms568203@gmail.com>
2020-06-19 08:58:14 +0530

    Added: Tc1.2_Given 0 Feet and Null Returns Not Equal

--- NUnitTestQuantityMeasurement/QuantityMeasurementTestCases.cs
+++ NUnitTestQuantityMeasurement/QuantityMeasurementTestCases.cs
@@ -20,5 +20,13 @@ namespace NUnitTestQuantityMeasurement
             Feet secondFeet = new Feet(0.0);
             Assert.AreEqual(firstFeet,secondFeet);
         }
+
+        [Test]
+        public void Given0FeetAnd1Feet_ShouldReturnsNotEqual()
+        {
+            Feet firstFeet = new Feet(0.0);
+            Feet secondFeet = new Feet(1.0);
+            Assert.AreNotEqual(firstFeet, secondFeet);
+        }
     }
 }
