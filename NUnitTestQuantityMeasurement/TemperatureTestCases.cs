1c22cd191e6d2a268355fe691b0fc7977e68a885
Mehboob750 <ms568203@gmail.com>
2020-06-20 18:05:10 +0530

    Added: Tc8.1_Given 0Fahrenheit and 0Fahrenheit checked Return Equal

---
+++ NUnitTestQuantityMeasurement/TemperatureTestCases.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using QuantityMeasurementProblem;
+
+namespace NUnitTestQuantityMeasurement
+{
+    class TemperatureTestCases
+    {
+        private Temperature fahrenheit = null;
+       
+        [SetUp]
+        public void Setup()
+        {
+            this.fahrenheit = new Temperature();
+        }
+
+        /// <summary>
+        /// Given 0 Fahrenheit And 0 Fahrenheit Returns the Equal
+        /// </summary>
+        [Test]
+        public void Given0FahrenheitAnd0Fahrenheit_ShouldReturnsEqual()
+        {
+            double firstFahrenheitValue = fahrenheit.ConvertValueToCelsius(Temperature.Unit.Fahrenheit, 0.0);
+            double secondFahrenheitValue = fahrenheit.ConvertValueToCelsius(Temperature.Unit.Fahrenheit, 0.0);
+            Assert.AreEqual(firstFahrenheitValue, secondFahrenheitValue);
+        }
+    }
+}
---
+++ QuantityMeasurementProblem/Temperature.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurementProblem
+{
+    public class Temperature
+    {
+        public enum Unit
+        {
+            Fahrenheit,
+            FahrenheitToCelsius
+        }
+
+        public double ConvertValueToCelsius(Unit unit, double value)
+        {
+            try
+            {
+                if (unit.Equals(Unit.FahrenheitToCelsius))
+                {
+                    return value * (100/212);
+                }
+
+                return value;
+            }
+            catch (Exception e)
+            {
+                throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.InvalidValue, e.Message);
+            }
+        }
+    }
+}
