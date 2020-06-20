9d9aea57c819f3a79b8d7aa4e89badbb9792158d
Mehboob750 <ms568203@gmail.com>
2020-06-20 15:18:54 +0530

    Added: Tc5.1_Given 0Gallon and 0Gallon When Check Return Equal

---
+++ NUnitTestQuantityMeasurement/VolumeTestCases.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using QuantityMeasurementProblem;
+
+namespace NUnitTestQuantityMeasurement
+{
+    class VolumeTestCases
+    {
+        private Volume litre = null;
+        private Volume gallon = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.litre = new Volume();
+            this.gallon = new Volume();
+        }
+
+        /// <summary>
+        /// Given Zero gallon And Zero gallon Returns the Equal
+        /// </summary>
+        [Test]
+        public void Given0GallonAnd0Gallon_ShouldReturnsEqual()
+        {
+            double firstGallonValue = gallon.ConvertValueToInch(Volume.Unit.Gallon, 0.0);
+            double secondGallonValue = gallon.ConvertValueToInch(Volume.Unit.Gallon, 0.0);
+            Assert.AreEqual(firstGallonValue, secondGallonValue);
+        }
+    }
+}
---
+++ QuantityMeasurementProblem/Volume.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantityMeasurementProblem
+{
+    public class Volume
+    {
+        public enum Unit
+        {
+            Gallon,
+            Litre,
+            GallonToLitre
+        }
+
+        public double ConvertValueToInch(Unit unit, double value)
+        {
+            try
+            {
+                if (unit.Equals(Unit.GallonToLitre))
+                {
+                    return value * 3.78;
+                }
+               
+                return value;
+            }
+            catch (Exception e)
+            {
+                throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.InvalidValue, e.Message);
+            }
+        }
+    }
+}
